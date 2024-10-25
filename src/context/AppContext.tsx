5edf9155a9eddb7cca75d14fc50f87a04cd37787
zlzcty <zlzcty@example.com>
2024-10-24 17:09:14 -0700

    form fields are reactive to typing

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -4,28 +4,28 @@ import { Expense } from "../types/types";
 // Exercise: Create add budget to the context
 
 interface AppContextType {
-  expenses: Expense[];
-  setExpenses: React.Dispatch<React.SetStateAction<Expense[]>>;
+    expenses: Expense[];
+    setExpenses: React.Dispatch<React.SetStateAction<Expense[]>>;
 }
 
 const initialState: AppContextType = {
-  expenses: [],
-  setExpenses: () => {},
+    expenses: [],
+    setExpenses: () => { },
 };
 
 export const AppContext = createContext<AppContextType>(initialState);
 
 export const AppProvider = (props: any) => {
-  const [expenses, setExpenses] = useState<Expense[]>(initialState.expenses);
+    const [expenses, setExpenses] = useState<Expense[]>(initialState.expenses);
 
-  return (
-    <AppContext.Provider
-      value={{
-        expenses: expenses,
-        setExpenses: setExpenses,
-      }}
-    >
-      {props.children}
-    </AppContext.Provider>
-  );
+    return (
+        <AppContext.Provider
+            value={{
+                expenses: expenses,
+                setExpenses: setExpenses,
+            }}
+        >
+            {props.children}
+        </AppContext.Provider>
+    );
 };
