8b546836f40af3f458e08138e24be0ef6af78d46
zlzcty <zlzcty@example.com>
2024-10-24 17:08:54 -0700

    form fields are reactive to typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { MyBudgetTracker } from "./views/MyBudgetTracker";
+import { AppProvider } from "./context/AppContext";
 
 const App = () => {
-  // HINT: Wrap the MyBudgetTracker component with AppContextProvider
-  return <MyBudgetTracker />;
+    // HINT: Wrap the MyBudgetTracker component with AppContextProvider
+
+    return (
+        <AppProvider>
+            <MyBudgetTracker />;
+        </AppProvider>
+    );
 };
 
 export default App;
--- src/components/Expense/AddExpenseForm.tsx
+++ src/components/Expense/AddExpenseForm.tsx
@@ -1,48 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { AppContext } from "../../context/AppContext";
 const AddExpenseForm = () => {
-  // Exercise: Consume the AppContext here
+	// Exercise: Consume the AppContext here
 
-  // Exercise: Create name and cost to state variables
+	const { expenses, setExpenses } = useContext(AppContext)
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+	// Exercise: Create name and cost to state variables
+	const [name, setName] = useState("");
+	const [cost, setCost] = useState("");
 
-    // Exercise: Add add new expense to expenses context array
-  };
+	const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setName(event.target.value)
+	}
 
-  return (
-    <form onSubmit={(event) => onSubmit(event)}>
-      <div className="row">
-        <div className="col-sm">
-          <label htmlFor="name">Name</label>
-          <input
-            required
-            type="text"
-            className="form-control"
-            id="name"
-            value={""}
-            // HINT: onChange={}
-          ></input>
-        </div>
-        <div className="col-sm">
-          <label htmlFor="cost">Cost</label>
-          <input
-            required
-            type="text"
-            className="form-control"
-            id="cost"
-            value={0}
-            // HINT: onChange={}
-          ></input>
-        </div>
-        <div className="col-sm">
-          <button type="submit" className="btn btn-primary mt-3">
-            Save
-          </button>
-        </div>
-      </div>
-    </form>
-  );
+	const handleCostChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setCost(event.target.value)
+	}
+
+	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		// Exercise: Add add new expense to expenses context array
+	};
+
+	return (
+		<form onSubmit={(event) => onSubmit(event)}>
+			<div className="row">
+				<div className="col-sm">
+					<label htmlFor="name">Name</label>
+					<input
+						required
+						type="text"
+						className="form-control"
+						id="name"
+						value={name}
+						onChange={handleNameChange}
+					></input>
+				</div>
+				<div className="col-sm">
+					<label htmlFor="cost">Cost</label>
+					<input
+						required
+						type="text"
+						className="form-control"
+						id="cost"
+						value={cost}
+						onChange={handleCostChange}
+					></input>
+				</div>
+				<div className="col-sm">
+					<button type="submit" className="btn btn-primary mt-3">
+						Save
+					</button>
+				</div>
+			</div>
+		</form>
+	);
 };
 
 export default AddExpenseForm;
