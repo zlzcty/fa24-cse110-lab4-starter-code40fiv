e5708b018869a098962cb4ea1f34a282161e069e
zlzcty <zlzcty@example.com>
2024-10-24 17:21:15 -0700

    Done 2.1.3: Add functionality to delete expenses

--- src/components/Expense/ExpenseItem.tsx
+++ src/components/Expense/ExpenseItem.tsx
@@ -1,10 +1,16 @@
+import React, { useContext } from "react";
 import { Expense } from "../../types/types";
+import { AppContext } from "../../context/AppContext";
 
 const ExpenseItem = (currentExpense: Expense) => {
-  // Exercise: Consume the AppContext here
+
+  const { expenses, setExpenses } = useContext(AppContext)
 
   const handleDeleteExpense = (currentExpense: Expense) => {
     // Exercise: Remove expense from expenses context array
+
+    setExpenses(expenses.filter(expense => expense.id !== currentExpense.id));
+
   };
 
   return (
